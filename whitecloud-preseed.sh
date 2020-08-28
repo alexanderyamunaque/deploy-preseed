837f22fd98aafee9639a8bef87a9ff7c340a95d5
Alexander Yamunaque <alexander.yamunaque@gmail.com>
2020-08-28 02:02:31 -0500

    First Commit

---
+++ whitecloud-preseed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export IMAGE_NAME=ayamunaque/preseed
+export TAG=1.3
+export UBUNTU="http://cdimage.ubuntu.com/ubuntu-server/bionic/daily/current/bionic-server-amd64.iso"
+
+echo "Downloading Docker Image:"
+docker pull ${IMAGE_NAME}:${TAG}
+
+sudo mkdir -p /etc/preseed/base_image
+sudo mkdir -p /etc/preseed/new_image
+
+read -p "Do you want to download the last version of Ubuntu Bionic? (y/n) `echo $'\n> '`" answer
+if [ "$answer" == "y" ]; then
+  echo "Downloading Ubuntu Image"
+  sudo wget $UBUNTU -P /etc/preseed/base_image
+else
+  echo "Executing docker container"
+fi
+
+sudo docker run --rm -it --privileged --name generator-iso -v /etc/preseed:/etc/preseed $IMAGE_NAME:$TAG python3 /root/main.py
